5614d2a077b130654143c74632b0324d6c2b355b
Aadit Shah <aadit2805@gmail.com>
2024-06-30 01:35:22 -0400

    swapped api

---
+++ README.md
@@ -0,0 +1,25 @@
+# Weather Text
+Automates morning texts of the daily weather forecast.
+
+## Setup
+1. Create environment variables for .env file
+  - API Key from [OpenWeatherMap](https://openweathermap.org/api/one-call-3) (specifically One Call API 3.0, very cheap)
+  - Personal phone number
+  - Account SID, Auth Token, and Twilio phone number from [Twilio](twilio.com)
+2. Clone this repository.
+3. Set up virtual environment and install the requirements:
+  ```bash
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
+```
+4. Update your shell script with the proper file paths to your virtual environment, interpreter, and script.
+5. Create cron job, you can use these [instructions](https://medium.com/@jameshamann/automation-with-cron-d10f7cbbb638). It should point towards the path of your shell script.
+
+There you have it! Right now, your computer will have to be on in order for the cron job to work, so I'm looking into ways to change this. 
+
+## To Do
+- [x] Utilize new weather API to access 24 hour forecasts
+- [ ] Integrate OpenAI API to pull weather data and get insights on what to wear for the day
+- [ ] Host on DigitalOcean
+- [ ] Build out frontend with dropdown options for users
---
+++ script.py
@@ -0,0 +1,68 @@
+import os
+import requests
+from twilio.rest import Client
+from dotenv import load_dotenv
+from datetime import date
+import geocoder
+
+# load .env file
+load_dotenv()
+
+# assign keys to variables
+api_key = os.getenv('API_KEY')
+sid = os.getenv('SID')
+token = os.getenv('TOKEN')
+to_num = os.getenv('TO_NUM')
+from_num = os.getenv('FROM_NUM')
+
+#need for api call
+geo = geocoder.ip('me') 
+lat = geo.latlng[0]
+lon = geo.latlng[1]
+city = geo.city
+
+today = date.today().isoformat() #need for api call
+
+# get weather data
+def get_weather(api_key, lat, lon, date):
+    try:
+        url = f'https://api.openweathermap.org/data/3.0/onecall/day_summary?lat={lat}&lon={lon}&date={date}&units=imperial&appid={api_key}'
+        response = requests.get(url)
+        data = response.json()  # grabs data
+
+        # extract data
+        weather = {
+            'max_temp': data['temperature']['max'],
+            'min_temp': data['temperature']['min'],
+            'morning': data['temperature']['morning'],
+            'afternoon': data['temperature']['afternoon'],
+            'evening': data['temperature']['evening'],
+            'night': data['temperature']['night'], 
+            'rain': data['precipitation']['total'],
+        }
+        return weather
+    except Exception as e:
+        print(f"failed to retrieve weather data: {e}")
+        return None
+
+# send the twilio sms
+def send_sms(sid, auth_token, to_num, from_num, msg):
+    try:
+        client = Client(sid, auth_token)  # creates the client object
+        message = client.messages.create(  # Twilio function, takes in 3 parameters (text, from, to)
+            body=msg,
+            from_=from_num,
+            to=to_num
+        )
+    except Exception as e:
+        print(f"failed to send SMS: {e}")
+        return None
+
+# calls the sms function and inputs the data to send the message
+def send_msg():
+    weather = get_weather(api_key, lat, lon, today)
+    if weather:
+        msg = f"Today's weather summary: Max Temperature: {weather['max_temp']}°F, Min Temperature: {weather['min_temp']}°F, Morning Temperature: {weather['morning']}°F, Afternoon Temperature: {weather['afternoon']}°F, Evening Temperature: {weather['evening']}°F, Night Temperature: {weather['night']}°F, Rainfall: {weather['rain']} inches."
+        send_sms(sid, token, to_num, from_num, msg)
+
+send_msg()
