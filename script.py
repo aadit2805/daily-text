9c42c4b7d68ecb1f09187c7addc5b778999ef94f
Aadit Shah <aadit2805@gmail.com>
2024-07-01 12:11:36 -0400

    added gpt api for clothing

--- script.py
+++ script.py
@@ -4,6 +4,7 @@ from twilio.rest import Client
 from dotenv import load_dotenv
 from datetime import date
 import geocoder
+from openai import OpenAI
 
 # load .env file
 load_dotenv()
@@ -15,7 +16,9 @@ token = os.getenv('token')
 to_num = os.getenv('to_num')
 from_num = os.getenv('from_num')
 city = os.getenv('city')
-today = date.today().isoformat()  # need for api call
+gpt_key = os.getenv('gpt_key')
+
+today = date.today().isoformat()
 
 # get latitude and longitude of the city
 def get_coords(city):
@@ -54,6 +57,7 @@ def get_weather(api_key, lat, lon, date):
     except Exception as e:
         return None
 
+
 # send the twilio sms
 def send_sms(sid, auth_token, to_num, from_num, msg):
     try:
@@ -66,12 +70,34 @@ def send_sms(sid, auth_token, to_num, from_num, msg):
     except Exception as e:
         return None
 
+# function to call GPT API
+def get_gpt(model_name, system_msg, user_msg):
+    client = OpenAI(api_key=gpt_key)
+    response = client.completions.create(
+        model=model_name,
+        prompt=f"{system_msg} {user_msg}",
+        max_tokens=2500
+    )
+    return response.choices[0].text.strip()
+
 # calls the sms function and inputs the data to send the message
 def send_msg():
     if lat and lon:
         weather = get_weather(api_key, lat, lon, today)
         if weather:
-            msg = f"Today's weather summary for {city}: \n\nMax Temperature: {weather['max_temp']}°F\nMin Temperature: {weather['min_temp']}°F\nMorning Temperature: {weather['morning']}°F\nAfternoon Temperature: {weather['afternoon']}°F\nEvening Temperature: {weather['evening']}°F\nNight Temperature: {weather['night']}°F\nRainfall: {weather['rain']} inches\nHumidity: {weather['humidity']}%."
+            system_msg = "You are a helpful assistant who is being given information about the weather, and will help with picking out someone's outfit based on the weather."
+            user_msg = f"What should I wear given this weather data: {weather}"
+            gpt_msg = get_gpt("gpt-3.5-turbo-instruct", system_msg, user_msg)
+            msg = (f"Today's weather summary for {city}:\n\n"
+                   f"High: {weather['max_temp']}°F\n"
+                   f"Low: {weather['min_temp']}°F\n"
+                   f"Morning: {weather['morning']}°F\n"
+                   f"Afternoon: {weather['afternoon']}°F\n"
+                   f"Evening: {weather['evening']}°F\n"
+                   f"Night: {weather['night']}°F\n"
+                   f"Rainfall: {weather['rain']} inches\n"
+                   f"Humidity: {weather['humidity']}%\n\n"
+                   f"What you should wear: {gpt_msg}")
             send_sms(sid, token, to_num, from_num, msg)
     else:
         return None
